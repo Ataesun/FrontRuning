a9cd4c7012ee02d4c6dc1e0798c786552c0536ee
CheerUpButterCup <62089371+CheerUpButterCup@users.noreply.github.com>
2021-01-02 21:09:08 +1030

    Wokring up untill ether loss is
    When Running the script now it goes untill slippage and ether loss is calcuated

--- app/isProfitable.js
+++ app/isProfitable.js
@@ -1,8 +1,8 @@
-const { ChainId,Fetcher, Route, Trade, TokenAmount, TradeType, Token } = require('@uniswap/sdk');
+const { ChainId, Fetcher, Route, Trade, TokenAmount, TradeType, Token } = require('@uniswap/sdk');
 const axios = require('axios').default
 const uniswap = require('./uniswap')
 
-const getDecimal = async (token) =>{
+const getDecimal = async (token) => {
     let sig = await axios.get(`https://api.ethplorer.io/getTokenInfo/${token}?apiKey=freekey`);
     return sig.data.decimals
 }
@@ -10,28 +10,25 @@ const getDecimal = async (token) =>{
 const fetchData = async (filteredTransaction) => {
     let token = new Token(ChainId.MAINNET, filteredTransaction.tokenOut, filteredTransaction.decimals)
     let pair = {};
+    let sell
     if (filteredTransaction.hasOwnProperty('sellAmountOutMin')) {
         pair = await Fetcher.fetchPairData(uniswap.weth, token);
-        return createTrade({
-            token,
-            pair,
-        }, /* Value needs to be replaced with calculated value */1e18,
-        filteredTransaction)
+        sell = true;
+        return createTrade(token,pair, /* Value needs to be replaced with calculated value */filteredTransaction.sellAmountOutMin, sell)
     } else {
         pair = await Fetcher.fetchPairData(token, (uniswap.weth));
-        return createTrade({
-            token,
-            pair,
-        },filteredTransaction.sellAmountOutMin
-        ,filteredTransaction)
+        sell = false;
+
+        return createTrade(token, pair, 1e18, sell)
     }
 
 }
 
-const createTrade = async (token, pair, amount, filteredTransaction) => {
+const createTrade = async (token, pair, amount, sell) => {
     let route = {};
     let ta = {};
-    if (filteredTransaction.hasOwnProperty('sellAmountOutMin')) {
+
+    if (sell) {
         route = new Route([pair], token);
         ta = new TokenAmount(token, BigInt(amount))
     } else {
@@ -43,19 +40,21 @@ const createTrade = async (token, pair, amount, filteredTransaction) => {
 }
 
 const isProfitable = async (filteredTransaction) => {
-    
+
     filteredTransaction.decimals = await getDecimal(filteredTransaction.tokenOut);
     console.log(filteredTransaction)
     let trade = await fetchData(filteredTransaction)
-
-    let MaximumEtherLoss = parseInt(obj.etherValue) - parseFloat(trade.executionPrice.invert().toSignificant(18)) 
-        * parseInt(filteredTransaction.amountOutMin).toSignificant(filteredTransaction.decimals)
+    let etherValue = parseInt(filteredTransaction.etherValue);
+    let executionPrice = parseFloat(trade.executionPrice.invert().toSignificant(18))
+    let Amount = parseInt(filteredTransaction.amountOutMin)
+    let MaximumEtherLoss =  etherValue - executionPrice*Amount.toPrecision(filteredTransaction.decimals)
     let slippagePercent = 1 - (MaximumEtherLoss / filteredTransaction.etherValue);
     // let input = (slippagePercent/2)
     console.log(`maximum Ether Loss ${MaximumEtherLoss / 1e18}`)
     console.log(`slippage percent is : ${slippagePercent}`);
 
-    let gasCost = filteredTransaction.gas*1.25 / 2e9
+    process.exit()
+    let gasCost = filteredTransaction.gas * 1.25 / 2e9
 
     // if ( gasCost > 0) {
     //     let tx = await uniswap.buyTokens(filteredTransaction);
