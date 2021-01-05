e2579905bd89e02eb4de4a84550afdf7654d1e96
CheerUpButterCup <62089371+CheerUpButterCup@users.noreply.github.com>
2021-01-05 16:23:47 +1030

    Update maximise.js

--- app/maximise.js
+++ app/maximise.js
@@ -1,35 +1,29 @@
-
-
 const maximise = async(eth,pairToken,threshHold) =>{
-    let ret = 0;
-    let currentPrice = pairToken.price
-    console.log(`pairToken price is ${currentPrice}`)
-    let maximumPrice = currentPrice*threshHold
-    console.log(`The price can increase up untill ${maximumPrice}`)
-    let ethPriceRise = eth.price-(maximumPrice*pairToken.reserve)
-    console.log(`newETH price = ${ethPriceRise}`)
-    let ethBuy = ethPriceRise/pairToken.reserve
-    console.log(`amount of ether to buy ${ethBuy}`)
+    let newPrice = threshHold*eth.price;  
+    console.log(newPrice)
+    let x = Number.parseFloat((pairToken.reserve-(newPrice*eth.reserve))/pairToken.reserve-(newPrice*eth.price))
+    console.log(x)
 
-    process.exit()
+
+
     // return ethBuy
     // using the reserve ratio, calcuate a value to take from one to the other such that the thre
 }
 
 module.exports = maximise
-// const test = () =>{
-//     let eth ={
-//         reserve : 4,
-//         price : 30,
-//     }
-//     let pairToken = {
-//         reserve : 120,
-//         price : 0.033,
-//     }
+const test = () =>{
+    let eth ={
+        reserve : 7,
+        price : 7/1400,
+    }
+    let pairToken = {
+        reserve : 1400,
+        price : 1400/7,
+    }
 
-//     let threshHold = 1.04;
+    let threshHold = 2;
 
-//     maximise(eth,pairToken,threshHold)
-// }
+    maximise(eth,pairToken,threshHold)
+}
 
-// test()
+test()
